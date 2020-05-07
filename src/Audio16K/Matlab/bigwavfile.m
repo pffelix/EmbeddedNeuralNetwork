dc327bbe847c07f6625de0e4e13e15ee86783aaf
Felix <pffelix@student.ethz.ch>
2020-05-07 15:27:30 +0200

    wrote code to generate one large WAv file and read it in

--- src/Audio16K/Matlab/bigwavfile.m
+++ src/Audio16K/Matlab/bigwavfile.m
@@ -4,8 +4,10 @@ ir_version = "1";
 ir_angles = 0:45:359; 
 speech_types = ["cmu_us_bdl_arctic", "cmu_us_jmk_arctic", "cmu_us_ksp_arctic", "cmu_us_rms_arctic", "cmu_us_slt_arctic", "cmu_us_clb_arctic"];
 mic_N = 2;
+ang_N = 8;
 fs = 16000;
-max_length = 113599;
+max_filesamples = 113599;
+max_files = 1132;
 
 %% run
 for j=1:length(speech_types)
@@ -17,25 +19,29 @@ for j=1:length(speech_types)
     speech_files = dir(filepath_speech);
     speech_files = {speech_files.name};
     speech_files = speech_files(3:end);
-    speech_files_N = length(speech_files);
-    data = zeros(max_length * speech_files_N, mic_N);
-    for i=1:length(ir_angles)
-        %% for every speech file
-        fprintf("process:" + "\n");
-        for speech_file_n = 1:speech_files_N
-            fprintf(speech_file_n + "/" + speech_files_N + "\n");
+    speech_files_N = max_files;
+    data = zeros(max_filesamples * speech_files_N * ang_N, mic_N);
+    output_file_n = 0;
+    fprintf("process:" + speech_types(j) + "\n");
+    for speech_file_n = 1:speech_files_N
+        fprintf(speech_file_n + "/" + speech_files_N + "\n");
+        for i=1:length(ir_angles)
+            %% for every speech file
+            output_file_n = output_file_n + 1;
             
             %% load speech file
             filename_ir = ir_type + "_" + ir_version + "_" + ir_angles(i) + "degree";
-            irspeech = audioread(filepath_speech + filename_speech + "_" + speech_file_n + "_" + filename_ir + ".wav");
+            try
+                irspeech = audioread(filepath_speech + filename_speech + "_" + speech_file_n + "_" + filename_ir + ".wav");
+            catch
+                break
+            end
             speech_N = length(irspeech);
-            index_1 = ((speech_file_n-1) * max_length) + 1;
-            index_2 = ((speech_file_n) * max_length);
-            data(index_1:index_1 + speech_N - 1, :) = irspeech;
+            index = ((output_file_n - 1) * max_filesamples) + 1;
+            data(index:index + speech_N - 1, :) = irspeech;
         end
-        
-        %% output
-        audiowrite(filepath_speech + "all.wav", data, fs);
 
     end
+    %% output
+    audiowrite(filepath_speech + "all.wav", data, fs);    
 end
--- src/PythonCode/develope.py
+++ src/PythonCode/develope.py
@@ -1,6 +1,7 @@
 # Import the necessary packages
 import os
 import numpy as np
+import numpy.matlib
 import math
 from itertools import *
 import more_itertools
@@ -8,59 +9,90 @@ from scipy import signal
 from scipy.fftpack import fftshift
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
-
+ 
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
 from keras import layers, models
 from keras import utils
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D,BatchNormalization,GRU,Bidirectional,Reshape
 
+# parameters
 fs = 16000;
+filesN = 1132;
+angleN = 8;
+micN= 2;
+samplesN = 113599;
 windowIncrease = 0.1 #in sec
 windowSize = 5 #in sec
 filterOrder = 10
 filterBand = [200,5000] #in Hertz
-folderName = ["cmu_us_awb_arctic", "cmu_us_bdl_arctic", "cmu_us_clb_arctic", "cmu_us_jmk_arctic", "cmu_us_ksp_arctic", "cmu_us_rms_arctic", "cmu_us_slt_arctic"]
+folderName = ["cmu_us_bdl_arctic", "cmu_us_awb_arctic", "cmu_us_clb_arctic", "cmu_us_jmk_arctic", "cmu_us_ksp_arctic", "cmu_us_rms_arctic", "cmu_us_slt_arctic"]
 folderPath = "./../../../recording/speech/" + str(math.ceil(fs/1000)) + "khz" + "/"
 #audioCh1Data = np.ndarray([0,windowSize*fs])
 #audioCh2Data = np.ndarray([0,windowSize*fs])
-#Y = np.ndarray([0,2])
+readBatchOn = True;
+filterOn = False;
+
+# init
 audioCh1Data = []
 audioCh2Data = []
 Y = []
 count = 0
 
 for folder in folderName:
-    for i in os.listdir(folderPath + folder):           # iteration over all data files
-        
+    if readBatchOn:
         # get meta data
-        deg = int(i[i.rfind("_")+1:i.find("degree")])/180*math.pi # read angle
-        dist = 1 # read distance
-        number = int(i[i.rfind("arctic")+7:i.find(str(math.ceil(fs/1000)) + "k")-1]) # read file number
+        deg = np.linspace(0, 315, 8)
+        deg = np.matlib.repmat(deg, 1, filesN).transpose()
+        dist = np.full((filesN * angleN, 1), 1)
+        number = np.linspace(1, filesN * angleN, filesN * angleN);
         
         # read data file
-        fs, data = wavfile.read(folderPath + folder + "/" + i)
-        tempData = data
-        audioCh1 = data[:,0]
-        audioCh2 = data[:,1]
+        print ("read data file")
+        fs, data = wavfile.read(folderPath + folder + "/" + "all.wav")
+        print ("finish data file")
         
         # filter
-        #sos = signal.butter(filterOrder, filterBand, 'bp', fs=fs, output='sos')
-        #audioCh1 = signal.sosfilt(sos, audioCh1)
-        #audioCh2 = signal.sosfilt(sos, audioCh2)
-    
-        # window (rectangular)
-        tempData = list(more_itertools.windowed(audioCh1, n=int(windowSize*fs), step=int(windowIncrease*fs)))
-        tempData.pop()
-        audioCh1Data.extend(tempData)
-    
-        tempData = list(more_itertools.windowed(audioCh2, n=int(windowSize*fs), step=int(windowIncrease*fs)))
-        tempData.pop()
-        audioCh2Data.extend(tempData)
-    
-        # resphape into one array
-        dataSize = (np.shape(tempData))[0]
-        Y.extend(list(np.repeat([dist,deg],dataSize))) 
-    
-        # log
-        print (i)
+        if filterOn:
+            sos = signal.butter(filterOrder, filterBand, 'bp', fs=fs, output='sos')
+            data[:,0] = signal.sosfilt(sos, data[:,0])
+            data[:,1] = signal.sosfilt(sos, data[:,1])
+            
+        # reshape into invidual wav files
+        data = np.reshape(data, (samplesN, filesN, angleN, micN))
+        
+        
+    else:
+        for i in os.listdir(folderPath + folder):           # iteration over all single data files
+            
+            # get meta data
+            deg = int(i[i.rfind("_")+1:i.find("degree")])/180*math.pi # read angle
+            dist = 1 # read distance
+            number = int(i[i.rfind("arctic")+7:i.find(str(math.ceil(fs/1000)) + "k")-1]) # read file number
+            
+            # read data file
+            tempData = data
+            audioCh1 = data[:,0]
+            audioCh2 = data[:,1]
+            
+            # filter
+            if filterOn:
+                sos = signal.butter(filterOrder, filterBand, 'bp', fs=fs, output='sos')
+                audioCh1 = signal.sosfilt(sos, audioCh1)
+                audioCh2 = signal.sosfilt(sos, audioCh2)
+        
+            # window (rectangular)
+            tempData = list(more_itertools.windowed(audioCh1, n=int(windowSize*fs), step=int(windowIncrease*fs)))
+            tempData.pop()
+            audioCh1Data.extend(tempData)
+        
+            tempData = list(more_itertools.windowed(audioCh2, n=int(windowSize*fs), step=int(windowIncrease*fs)))
+            tempData.pop()
+            audioCh2Data.extend(tempData)
+        
+            # resphape into one array
+            dataSize = (np.shape(tempData))[0]
+            Y.extend(list(np.repeat([dist,deg],dataSize))) 
+        
+            # log
+            print (i)
